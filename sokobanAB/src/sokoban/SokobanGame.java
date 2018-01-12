6c5d57012d0e7790494d2fb3c935b3d9f740e00d
Adam <adambar@live.co.uk>
2018-01-12 17:08:03 +0000

    Adding buttons on screen

--- sokobanAB/src/sokoban/SokobanGame.java
+++ sokobanAB/src/sokoban/SokobanGame.java
@@ -3,6 +3,7 @@ package sokoban;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
@@ -14,24 +15,62 @@ import javax.swing.WindowConstants;
 public class SokobanGame extends JComponent implements ActionListener {
     
     JFrame mainWindow;
+    Level currentLevel;
+    
+    public JButton upButton;
+    public JButton downButton;
+    public JButton leftButton;
+    public JButton rightButton;
     
     SokobanGame() {
 //  mainWindow is a new type of JFrame  
     mainWindow = new JFrame();
 //  size of window (main frame)  
-    mainWindow.setSize(400,400);
+    mainWindow.setSize(1100,800);
 //  won't be using layout manager  
     mainWindow.setLayout(null);
 //  set to visible to actually see frame
     mainWindow.setVisible(true);
 //  make program close after exit (saves on time)
     mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    }
+    
+//  button controls  
+    upButton = new JButton("UP");
+//  adds button to mainWindow  
+    mainWindow.add(upButton);
+    upButton.setBounds(850,10,100,30);
+    upButton.setVisible(true);
+    upButton.addActionListener(this);
+
+    downButton = new JButton("DOWN");
+//  adds button to mainWindow  
+    mainWindow.add(downButton);
+    downButton.setBounds(850,50,100,30);
+    downButton.setVisible(true);
+    downButton.addActionListener(this);
+    
+    leftButton = new JButton("LEFT");
+//  adds button to mainWindow  
+    mainWindow.add(leftButton);
+    leftButton.setBounds(740,50,100,30);
+    leftButton.setVisible(true);
+    leftButton.addActionListener(this);
+    
+    rightButton = new JButton("RIGHT");
+//  adds button to mainWindow  
+    mainWindow.add(rightButton);
+    rightButton.setBounds(960,50,100,30);
+    rightButton.setVisible(true);
+//  when button is clicked, an action is sent  
+    rightButton.addActionListener(this);
+
+    
+    
+    
+}
 //  used to stop error of actionlistener implementation for now  
     @Override
     public void actionPerformed(ActionEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
 }
